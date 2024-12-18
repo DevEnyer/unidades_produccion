19a89a71ffe7834b814a98dd7ee3fac55fc89e36
Enyer045 <enyerber045gmail.com>
2024-12-18 12:50:27 -0400

    otras partes

--- apps/auxiliares/apps.py
+++ apps/auxiliares/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AuxiliaresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auxiliares'
+    name = 'apps.auxiliares'
