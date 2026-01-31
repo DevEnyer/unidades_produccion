91658749dc78b5ff990b8423f66b728c24afc879
Enyer045 <enyerber045gmail.com>
2026-01-31 07:51:33 -0400

    fix: modularización de resource para reporte

--- apps/produccion/admin.py
+++ apps/produccion/admin.py
@@ -6,43 +6,12 @@ from django.utils.html import format_html
 from import_export import resources, fields
 from import_export.admin import ExportActionModelAdmin
 
+from apps.produccion.resource import UnidadProduccionResource
+
 from apps.produccion.models.almacenaje import Almacenaje
 from apps.produccion.models.produccion import Produccion
 from apps.produccion.models.unidad_produccion import UnidadProduccion, UnidadResponsable
 from apps.produccion.models.situacion_juridica import SituacionJuridica
-
-class UnidadProduccionResource(resources.ModelResource): # Clase para definir la exportación de datos con Import-Export
-    nombre = fields.Field(attribute='nombre', column_name='Nombre')
-    estado = fields.Field(attribute='estado__descripcion', column_name='Estado')
-    municipio = fields.Field(attribute='municipio__descripcion', column_name='Municipio')
-    parroquia = fields.Field(attribute='parroquia__descripcion', column_name='Parroquia')
-    direccion = fields.Field(attribute='direccion', column_name='Dirección')
-    tipos_establecimiento = fields.Field(attribute='tipos_establecimiento__descripcion', column_name='Tipo establecimiento')
-    descripcion_actividad = fields.Field(attribute='descripcion_actividad', column_name='Descripción de la actividad')
-    productividad_activa = fields.Field(attribute='productividad_activa', column_name='Productividad')
-    cantidad_trabajadores = fields.Field(attribute='cantidad_trabajadores', column_name='Trabajadores')
-    responsables = fields.Field(column_name='Responsables')
-    observaciones = fields.Field(attribute='observaciones', column_name='Observaciones')
-    class Meta:
-        model = UnidadProduccion
-        fields = (
-            'nombre', 
-            'estado', 
-            'municipio', 
-            'parroquia', 
-            'direccion', 
-            'tipos_establecimiento', 
-            'descripcion_actividad', 
-            'productividad_activa', 
-            'cantidad_trabajadores', 
-            'responsables', 
-            'observaciones'
-        )
-        export_order = fields
-    
-    def dehydrate_responsables(self, unidad):
-        # Aquí usamos el método que ya creaste inteligentemente en tu modelo
-        return unidad.get_responsables()
 
 class AdminUnidadResponsableInline(admin.TabularInline):
     model = UnidadResponsable
---
+++ apps/produccion/resource.py
@@ -0,0 +1,36 @@
+from import_export import resources, fields
+
+from apps.produccion.models.unidad_produccion import UnidadProduccion
+
+class UnidadProduccionResource(resources.ModelResource): # Clase para definir la exportación de datos con Import-Export
+    nombre = fields.Field(attribute='nombre', column_name='Nombre')
+    estado = fields.Field(attribute='estado__descripcion', column_name='Estado')
+    municipio = fields.Field(attribute='municipio__descripcion', column_name='Municipio')
+    parroquia = fields.Field(attribute='parroquia__descripcion', column_name='Parroquia')
+    direccion = fields.Field(attribute='direccion', column_name='Dirección')
+    tipos_establecimiento = fields.Field(attribute='tipos_establecimiento__descripcion', column_name='Tipo establecimiento')
+    descripcion_actividad = fields.Field(attribute='descripcion_actividad', column_name='Descripción de la actividad')
+    productividad_activa = fields.Field(attribute='productividad_activa', column_name='Productividad')
+    cantidad_trabajadores = fields.Field(attribute='cantidad_trabajadores', column_name='Trabajadores')
+    responsables = fields.Field(column_name='Responsables')
+    observaciones = fields.Field(attribute='observaciones', column_name='Observaciones')
+    class Meta:
+        model = UnidadProduccion
+        fields = (
+            'nombre', 
+            'estado', 
+            'municipio', 
+            'parroquia', 
+            'direccion', 
+            'tipos_establecimiento', 
+            'descripcion_actividad', 
+            'productividad_activa', 
+            'cantidad_trabajadores', 
+            'responsables', 
+            'observaciones'
+        )
+        export_order = fields
+    
+    def dehydrate_responsables(self, unidad):
+        # Aquí usamos el método que ya creaste inteligentemente en tu modelo
+        return unidad.get_responsables()
